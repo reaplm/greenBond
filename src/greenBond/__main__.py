b71955f6f517ecc103a991bba94ed5faa5c278f9
Pearl Molefe <pdm.molefe@gmail.com>
2026-06-03 22:28:14 +0100

    lading data

--- src/greenBond/__main__.py
+++ src/greenBond/__main__.py
@@ -1,5 +1,24 @@
+import pandas as pd
+from pathlib import Path
+
+def load_data():
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent
+    path = BASE_DIR / "data" / "green_n_transition_v1_clean.csv"
+
+    try:
+        df = pd.read_csv(path)
+        print("Successfully loaded data....")
+        return df
+    except FileNotFoundError:
+        print("Failed to load file. Please check location.")
+    
+    
+
 def main():
     print("Starting Green Bond Analysis.....")
+    
+    # Loading the data
+    green_benefit_df = load_data()
 
 if __name__ == "__main__":
     main()
